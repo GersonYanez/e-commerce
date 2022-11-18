ccac85666b7a914c56b21be9c00c22e7ab29c101
GersonYanez <gersonalbertoyanez@gmail.com>
2022-11-18 20:58:27 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'products', pathMatch:'full'},
+  {path:'products', component: ProductsComponent},
+  {path:'cart', component: CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
